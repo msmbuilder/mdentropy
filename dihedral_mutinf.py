a4a220c0e223f46252ceca6b13ad66bb4d43ff9b
Carlos Hernandez <cxh@stanford.edu>
2015-01-11 20:24:00 -0800

    Flake8

--- dihedral_mutinf.py
+++ dihedral_mutinf.py
@@ -1,28 +1,34 @@
 import numpy as np
 import mdtraj as md
-import argparse, cPickle
+import argparse
+import cPickle
 from multiprocessing import Pool
 from itertools import combinations_with_replacement as combinations
 from sklearn.metrics import mutual_info_score
 from contextlib import closing
 
+
 def rbins(n=30):
     return np.linspace(-np.pi, np.pi, n+3)[1:-1]
 
+
 def mi(X, Y, r=rbins()):
     H = np.histogram2d(X, Y, [r, r])[0]
-    return mutual_info_score(None, None, contingency = H)
-    
+    return mutual_info_score(None, None, contingency=H)
+
+
 def dihedrals(traj):
-    kinds = [md.compute_phi, 
+    kinds = [md.compute_phi,
              md.compute_psi]
     return [kind(traj)[1].T for kind in kinds]
-    
-def f(D): 
+
+
+def f(D):
     g = lambda i: sum([mi(d[0][i[0]], d[1][i[1]]) for d in combinations(D, 2)])
     g.__name__ = 'g'
     return g
-    
+
+
 def run(traj, iter, N):
     D = dihedrals(traj)
     n = D[0].shape[0]
@@ -30,28 +36,37 @@ def run(traj, iter, N):
     for i in range(iter+1):
         r = np.zeros((n, n))
         g = f(D)
-        
         with closing(Pool(processes=N)) as pool:
             r[np.triu_indices(n)] = pool.map(g, combinations(range(n), 2))
             pool.terminate()
         r[np.triu_indices(n)[::-1]] = r[np.triu_indices(n)]
         R.append(r)
         [np.random.shuffle(d) for d in D]
-    return R[0] - np.mean(R[1:], axis = 0)
-    
-    
+    return R[0] - np.mean(R[1:], axis=0)
+
+
 def parse_cmdln():
-    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('-i', '--input', dest='traj', help='File containing trajectory.')
-    parser.add_argument('-s', '--shuffle-iter', dest='iter', help='Number of shuffle iterations.', default=100, type=int)
-    parser.add_argument('-t', '--topology', dest='top', help='File containing topology.', default=None)
-    parser.add_argument('-n', '--n-proc', dest='N', help='Number of processors to be used.', default=4, type=int)
-    parser.add_argument('-o', '--output', dest='out', help='Name of output file.', default='mutinf.pkl')
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-i', '--input', dest='traj',
+                        help='File containing trajectory.')
+    parser.add_argument('-s', '--shuffle-iter', dest='iter',
+                        help='Number of shuffle iterations.',
+                        default=100, type=int)
+    parser.add_argument('-t', '--topology', dest='top',
+                        help='File containing topology.', default=None)
+    parser.add_argument('-n', '--n-proc', dest='N',
+                        help='Number of processors to be used.',
+                        default=4, type=int)
+    parser.add_argument('-o', '--output', dest='out',
+                        help='Name of output file.', default='mutinf.pkl')
     args = parser.parse_args()
     return args
-        
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     options = parse_cmdln()
-    traj = md.load(options.traj, top = options.top)
+    traj = md.load(options.traj, top=options.top)
     M = run(traj, options.iter, options.N)
     cPickle.dump(M, open(options.out, 'wb'))
--- dihedral_tent.py
+++ dihedral_tent.py
@@ -1,51 +1,65 @@
 import numpy as np
 import mdtraj as md
-import argparse, cPickle
+import argparse
+import cPickle
 from multiprocessing import Pool
 from itertools import product
 from itertools import combinations_with_replacement as combinations
 from contextlib import closing
 
+
 def rbins(n=30):
     return np.linspace(-np.pi, np.pi, n+3)[1:-1]
-    
+
+
 def ent(H):
     H /= H.sum()
     return -np.sum(H*np.nan_to_num(np.log2(H)))
-    
+
+
 def ent1D(X, r=rbins()):
     H = np.histogram(X, r)[0]
     return ent(H)
 
+
 def ent2D(X, Y, r=rbins()):
     H = np.histogram2d(X, Y, 2*[r])[0]
     return ent(H)
-    
+
+
 def ent3D(X, Y, Z, r=rbins()):
     W = np.vstack((X, Y, Z)).T
     H = np.histogramdd(W, 3*[r])[0]
     return ent(H)
-    
-def ce(X,Y):
+
+
+def ce(X, Y):
     return ent2D(X, Y) - ent1D(Y)
+
 
 def cmi(X, Y, Z):
     return ent2D(X, Z) + ent2D(Y, Z) - ent3D(X, Y, Z) - ent1D(Z)
-    
+
+
 def dihedrals(traj):
-    kinds = [md.compute_phi, 
+    kinds = [md.compute_phi,
              md.compute_psi]
     return [kind(traj)[1].T for kind in kinds]
-    
-def f(cD, pD): 
-    g = lambda i: sum([cmi(cD[d[0]][i[0]], pD[d[1]][i[1]], pD[d[0]][i[0]]) for d in combinations(range(len(cD)), 2)])
+
+
+def f(cD, pD):
+    g = lambda i: sum([cmi(cD[d[0]][i[0]], pD[d[1]][i[1]], pD[d[0]][i[0]])
+                      for d in combinations(range(len(cD)), 2)])
     g.__name__ = 'g'
     return g
-    
-def h(cD, pD): 
-    q = lambda i: sum([ce(cD[d[0]][i[0]], pD[d[0]][i[0]]) for d in combinations(range(len(cD)), 2)])
+
+
+def h(cD, pD):
+    q = lambda i: sum([ce(cD[d[0]][i[0]], pD[d[0]][i[0]])
+                       for d in combinations(range(len(cD)), 2)])
     q.__name__ = 'q'
     return q
+
 
 def run(current, past, iter, N):
     cD = dihedrals(current)
@@ -56,32 +70,44 @@ def run(current, past, iter, N):
     for i in range(iter+1):
         g = f(cD, pD)
         with closing(Pool(processes=N)) as pool:
-            R.append(np.reshape(pool.map(g, product(range(n), range(n))), (n, n)))
+            R.append(np.reshape(pool.map(g,
+                                         product(range(n), range(n))), (n, n)))
             pool.terminate()
         [np.random.shuffle(d) for d in cD]
         [np.random.shuffle(d) for d in pD]
-    CMI = R[0] - np.mean(R[1:], axis = 0)
+    CMI = R[0] - np.mean(R[1:], axis=0)
     with closing(Pool(processes=N)) as pool:
-        CH = (pool.map(q, zip(*(2*[range(n)])))*np.ones((n,n))).T
+        CH = (pool.map(q, zip(*(2*[range(n)])))*np.ones((n, n))).T
         pool.terminate()
     T = CMI/CH
     return T - T.T
-    
-    
+
+
 def parse_cmdln():
-    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('-c', '--current', dest='current',help='File containing current step states.')
-    parser.add_argument('-p', '--past', dest='past',help='File containing past step states.')
-    parser.add_argument('-t', '--topology', dest='top',help='File containing topology.', default=None)
-    parser.add_argument('-s', '--shuffle-iter', dest='iter', help='Number of shuffle iterations.', default=100, type=int)
-    parser.add_argument('-n', '--n-proc', dest='N',help='Number of processors', default=4, type=int)
-    parser.add_argument('-o', '--output', dest='out', help='Name of output file.', default='tent.pkl')
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-c', '--current', dest='current',
+                        help='File containing current step states.')
+    parser.add_argument('-p', '--past', dest='past',
+                        help='File containing past step states.')
+    parser.add_argument('-t', '--topology',
+                        dest='top', help='File containing topology.',
+                        default=None)
+    parser.add_argument('-s', '--shuffle-iter', dest='iter',
+                        help='Number of shuffle iterations.',
+                        default=100, type=int)
+    parser.add_argument('-n', '--n-proc', dest='N',
+                        help='Number of processors', default=4, type=int)
+    parser.add_argument('-o', '--output', dest='out',
+                        help='Name of output file.', default='tent.pkl')
     args = parser.parse_args()
     return args
-    
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     options = parse_cmdln()
-    current = md.load(options.current, top = options.top)
-    past = md.load(options.past, top = options.top)
+    current = md.load(options.current, top=options.top)
+    past = md.load(options.past, top=options.top)
     D = run(current, past, options.iter, options.N)
     cPickle.dump(D, open(options.out, 'wb'))
