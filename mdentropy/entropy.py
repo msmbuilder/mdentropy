17a5fedc9d0915dd181bdf7eb82d5580fe9d9aa8
Carlos Hernandez <cxh@stanford.edu>
2015-06-19 01:30:37 -0700

    pt bias

--- mdentropy/entropy.py
+++ mdentropy/entropy.py
@@ -6,10 +6,10 @@ from sklearn.metrics import mutual_info_score
 def ent(nbins, range, correction=True, *args):
     data = np.vstack((args)).T
     hist = np.histogramdd(data, bins=nbins, range=range)
-    dx = hist[1][0][1] - hist[1][0][0]
+    #dx = hist[1][0][1] - hist[1][0][0]
     H = stats.entropy(hist[0].flatten())
     if correction:
-        return H + data.shape[1]*np.log(dx)
+        return H + panzeritreves(hist[0].flatten(), data.shape[0])
     return H
 
 
@@ -27,3 +27,28 @@ def cmi(nbins, X, Y, Z, range=[-180., 180.]):
                 ent(nbins, 2*[range], Y, Z),
                 -ent(nbins, 3*[range], X, Y, Z),
                 - ent(nbins, [range], Z)])
+
+
+def panzeritreves(binprob, totaltrials):
+    totalbins = len(binprob)
+    eps = np.spacing(1)
+    nb = sum(binprob > eps)
+    if (nb < totalbins):
+        nb_x = nb - sum((binprob > eps) * (binprob < 1) * np.exp(np.log(1 - binprob + eps) * totaltrials))
+        delta_N_prev = totalbins
+        delta_N = abs(nb - nb_x)
+        xtr = 0
+        while (delta_N < delta_N_prev and ((nb+xtr) < totalbins)):
+            xtr += 1
+            nb_x = 0.0
+            gg = xtr*(1.-((totaltrials/(totaltrials+nb))**(1./totaltrials)))
+            qc_x = (1-gg) * (binprob*totaltrials+1) / (totaltrials+nb)
+            nb_x = np.sum((binprob > eps) * (1 - np.exp(np.log(1 - qc_x) * totaltrials)))
+            qc_x = gg / xtr
+            nb_x = nb_x + xtr*(1. - np.exp(np.log(1. - qc_x) * totaltrials))
+            delta_N_prev = delta_N
+            delta_N = abs(nb - nb_x)
+        nb += xtr - 1
+        if (delta_N < delta_N_prev):
+            nb += 1
+    return nb
