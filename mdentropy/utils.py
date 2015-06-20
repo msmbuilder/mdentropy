9fd2bdf8052bbf92b34c1d53e7d15d0b86b8164d
Carlos Hernandez <cxh@stanford.edu>
2015-06-19 20:13:10 -0700

    init

--- mdentropy/utils.py
+++ mdentropy/utils.py
@@ -8,13 +8,16 @@ import mdtraj as md
 
 class timing(object):
     "Context manager for printing performance"
+    def __init__(self, iter):
+        self.iter = iter
+
     def __enter__(self):
         self.start = time.time()
 
     def __exit__(self, ty, val, tb):
         end = time.time()
-        print("Runtime : %0.3f seconds" %
-              (end-self.start))
+        print("Round %d : %0.3f seconds" %
+              (self.iter, end-self.start))
         return False
 
 
