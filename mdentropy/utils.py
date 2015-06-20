0c4edb13c88d848d0062ebd3c7a41e49b8a444ed
Carlos Hernandez <cxh@stanford.edu>
2015-06-19 19:17:32 -0700

    reorganize

--- mdentropy/utils.py
+++ mdentropy/utils.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import time
 import numpy as np
 import pandas as pd
+import mdtraj as md
 
 
 class timing(object):
@@ -24,3 +25,26 @@ def shuffle(df, n=1):
         new_vals = df.take(sampler(df.shape[0])).values
         df = pd.DataFrame(new_vals, index=ind)
     return df
+
+
+class Dihedrals(object):
+    def __call__(self, traj):
+        atoms, angles = self.method(traj)
+        idx = [traj.topology.atom(i).residue.index
+               for i in atoms[:, self.type]]
+        return pd.DataFrame(180*angles/np.pi, columns=idx)
+
+    def __init__(self, method, type):
+        assert type < 3 or type > -1
+        self.type = type
+        self.method = method
+
+
+def dihedrals(traj):
+    kinds = [
+        Dihedrals(md.compute_phi, 2),
+        Dihedrals(md.compute_psi, 1),
+        Dihedrals(md.compute_chi1, 0),
+        Dihedrals(md.compute_chi2, 1)
+        ]
+    return [kind(traj) for kind in kinds]
