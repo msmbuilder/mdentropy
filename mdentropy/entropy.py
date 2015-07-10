59603e3462ff181b55a1b36cea3661a2c5993f9f
server user for FAH <server@vspd3.stanford.edu>
2015-07-09 23:28:41 -0700

    nan_to_num

--- mdentropy/entropy.py
+++ mdentropy/entropy.py
@@ -14,27 +14,27 @@ def entc(nbins, range, *args):
     N = args[0].shape[0]
     bins = hist(nbins, range, *args)
     return np.sum(bins*(np.log(N)
-                  - psi(bins)
+                  - np.nan_to_num(psi(bins))
                   - ((-1)**bins/(bins + 1))))/N
 
 
 def mi(nbins, X, Y, range=2*[[-180., 180.]]):
     bins = hist(nbins, range, X, Y)
-    return entc(nbins, range, X)
+    return (entc(nbins, range, X)
            + entc(nbins, range, Y)
-           - entc(nbins, range, X, Y)
+           - entc(nbins, range, X, Y))
 
 
 def ce(nbins, X, Y, range=[-180., 180.]):
-    return entc(nbins, 2*[range], X, Y)
-           - entc(nbins, [range], Y)
+    return (entc(nbins, 2*[range], X, Y)
+           - entc(nbins, [range], Y))
 
 
 def cmi(nbins, X, Y, Z, range=[-180., 180.]):
-    return sum([entc(nbins, 2*[range], X, Z),
-                entc(nbins, 2*[range], Y, Z),
-                - entc(nbins, [range], Z),
-                - entc(nbins, 3*[range], X, Y, Z)])
+    return (entc(nbins, 2*[range], X, Z)
+            +entc(nbins, 2*[range], Y, Z)
+            - entc(nbins, [range], Z)
+            - entc(nbins, 3*[range], X, Y, Z))
 
 
 def ncmi(nbins, X, Y, Z, range=[-180., 180.]):
