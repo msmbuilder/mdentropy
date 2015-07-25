1c2460b668df3ec57e267eb7ec720e3d3a075297
Carlos Hernández <cxh@stanford.edu>
2015-07-24 19:18:04 -0700

    add chaowangjost
    py3
    
    py3

--- mdentropy/entropy.py
+++ mdentropy/entropy.py
@@ -1,46 +1,70 @@
 import numpy as np
 from scipy import stats
-from scipy.special import psi
+from scipy.special import digamma
 from sklearn.metrics import mutual_info_score
 from mdentropy.utils import hist
 
 
-def ent(nbins, r, *args):
+def ent(nbins, r, method, *args):
     bins = hist(nbins, r, *args)
+    if method == 'chaowangjost':
+        return chaowangjost(bins)
+    elif method == 'grassberger':
+        return grassberger(bins)
     return stats.entropy(bins)
 
 
-def entc(nbins, r, *args):
-    N = args[0].shape[0]
-    bins = hist(nbins, r, *args)
+def grassberger(bins):
+    N = np.sum(bins)
     return np.sum(bins*(np.log(N)
-                  - np.nan_to_num(psi(bins))
-                  - ((-1)**bins/(bins + 1))))/N
+                  - np.nan_to_num(digamma(bins))
+                  - ((-1.)**bins/(bins + 1.))))/N
 
 
-def mi(nbins, X, Y, r=[-180., 180.]):
-    return (entc(nbins, [r], X)
-           + entc(nbins, [r], Y)
-           - entc(nbins, 2*[r], X, Y))
+def chaowangjost(bins):
+    N = int(np.sum(bins))
+    bc = np.bincount(bins.astype(int))
+    if bc[2] == 0:
+        if bc[1] == 0:
+            A = 1.
+        else:
+            A = 2./((N - 1.) * (bc[1] - 1.) + 2.)
+    else:
+        A = 2. * bc[2]/((N - 1.) * (bc[1] - 1.) + 2. * bc[2])
+    p = np.arange(1, N)
+    p = 1./p * (1 - A)**p
+    cwj = np.sum(bins/N * (digamma(N) - np.nan_to_num(digamma(bins))))
+    if bc[1]>0 and A != 1:
+        cwj += np.nan_to_num(bc[1]/N
+                             * (1 - A)**(1 - N * (-np.log(A) - np.sum(p))))
+    return cwj
 
 
-def nmi(nbins, X, Y, r=[-180., 180.]):
+def mi(nbins, X, Y, r=[-180., 180.], method = 'chaowangjost'):
+    return (ent(nbins, [r], method, X)
+           + ent(nbins, [r], method, Y)
+           - ent(nbins, 2*[r], method, X, Y))
+
+
+def nmi(nbins, X, Y, r=[-180., 180.], method = 'chaowangjost'):
     return (mi(nbins, X, Y, r = r)/
-            np.sqrt(entc(nbins, [r], X)*entc(nbins, [r], Y)))
+            np.sqrt(ent(nbins, [r], method, X)
+                    * ent(nbins, [r], method, Y)))
 
 
-def ce(nbins, X, Y, r=[-180., 180.]):
-    return (entc(nbins, 2*[r], X, Y)
-           - entc(nbins, [r], Y))
+def ce(nbins, X, Y, r=[-180., 180.], method = 'chaowangjost'):
+    return (ent(nbins, 2*[r], method, X, Y)
+           - ent(nbins, [r], method, Y))
 
 
-def cmi(nbins, X, Y, Z, r=[-180., 180.]):
-    return (entc(nbins, 2*[r], X, Z)
-            +entc(nbins, 2*[r], Y, Z)
-            - entc(nbins, [r], Z)
-            - entc(nbins, 3*[r], X, Y, Z))
+def cmi(nbins, X, Y, Z, r=[-180., 180.], method = 'chaowangjost'):
+    return (ent(nbins, 2*[r], method, X, Z)
+            +ent(nbins, 2*[r], method, Y, Z)
+            - ent(nbins, [r], method, Z)
+            - ent(nbins, 3*[r], method, X, Y, Z))
 
 
-def ncmi(nbins, X, Y, Z, r=[-180., 180.]):
-    return (1 + (entc(nbins, 2*[r], Y, Z)
-            - entc(nbins, 3*[r], X, Y, Z))/ce(nbins, X, Z, r=r))
+def ncmi(nbins, X, Y, Z, r=[-180., 180.], method = 'chaowangjost'):
+    return (1 + (ent(nbins, 2*[r], method, Y, Z)
+            - ent(nbins, 3*[r], method, X, Y, Z))
+            / ce(nbins, X, Z, r=r, method=method))
